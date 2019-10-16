79c18e3ac1cfd2126c94718220802532b56cb17b
root <root@localhost.localdomain>
2019-10-16 10:26:47 +0200

    test

--- utils.php
+++ utils.php
@@ -25,7 +25,7 @@ function query($query)
 	return $result;
 }
 
-function write_db($file)
+function insert_file($file)
 {
 	$tmp_file = file($file);
 	for($i = 0; $i < count($tmp_file); $i++)
@@ -33,10 +33,69 @@ function write_db($file)
 		$parts = explode(';', $tmp_file[$i]);
 		$n_puntate = $parts[0];
 		$id_utente = $parts[1];
-		$id_utente = $parts[1];
-		$id_utente = $parts[1];
+		$id_prodotto = $parts[2];
+		$tipo_puntata = $parts[3];
+		query("INSERT INTO bidoo_data (id_utente, id_prodotto, n_puntate, tipo_puntata) VALUES (\"" . $id_utente . "\", \"" . $id_prodotto . "\", " . $n_puntate . ", " . $tipo_puntata . ")");
 	}
 }
+
+function insert_line($string)
+{
+	$parts = explode(';', $string);
+	$n_puntate = $parts[0];
+	$id_utente = $parts[1];
+	$id_prodotto = $parts[2];
+	$tipo_puntata = $parts[3];
+	query("INSERT INTO bidoo_data (id_utente, id_prodotto, n_puntate, tipo_puntata) VALUES (\"" . $id_utente . "\", \"" . $id_prodotto . "\", " . $n_puntate . ", " . $tipo_puntata . ")");
+}
+
+function select_row($row_name)
+{
+	return query("SELECT " . $row_name . " FROM bidoo_data");
+}
+
+function create_html_table()
+{
+	echo "<table>";
+	echo "<tr>";
+	echo "<td>ID UTENTE</td>";
+	echo "<td>ID PRODOTTO</td>";
+	echo "<td>N PUNTATE</td>";
+	echo "<td>TIPO PUNTATA</td>";
+	echo "</tr>";
+	$id_utente = select_row("id_utente");
+	$id_prodotto = select_row("id_prodotto");
+	$n_puntate = select_row("n_puntate");
+	$tipo_puntata = select_row("tipo_puntata");
+
+	if($id_utente->num_rows > 0 && $id_prodotto->num_rows > 0 && $n_puntate->num_rows > 0 && $tipo_puntata->num_rows > 0)
+	{
+		$row1 = $id_utente->fetch_all();
+		$row2 = $id_prodotto->fetch_all();
+		$row3 = $n_puntate->fetch_all();
+		$row4 = $tipo_puntata->fetch_all();
+
+		for($i = 0; $i < count($row1); $i++)
+		{
+			echo "<tr>";
+			echo "<td>" . $row1[$i][0] . "</td>";
+			echo "<td>" . $row2[$i][0] . "</td>";
+			echo "<td>" . $row3[$i][0] . "</td>";
+			echo "<td>" . $row4[$i][0] . "</td>";
+			echo "</tr>";
+		}
+	}
+	echo "</table>";
+}
+
+create_html_table();
+
+/*
+insert_line("231;giannimario;193028374;3");
+insert_line("324;ivan;193028374;1");
+insert_line("45;osanna;193028389;1");
+insert_line("65;rinogino;193028390;3");
+*/
 
 /*
 PRELEVAMENTO DI DATI DAL DATABASE
