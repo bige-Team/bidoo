7097640cc6fe338ff3ee7cc88cc36cf5323947f8
federicodebona@hotmail.it <Rt9du2pg>
2019-11-10 22:26:05 +0100

    shared memory

--- threading.php
+++ threading.php
@@ -1,22 +1,5 @@
 
 <?php
 
-class workerThread extends Thread {
-public function __construct($i){
-  $this->i=$i;
-}
-
-public function run(){
-  while(true){
-   echo $this->i;
-   sleep(1);
-  }
-}
-}
-
-for($i=0;$i<50;$i++){
-$workers[$i]=new workerThread($i);
-$workers[$i]->start();
-}
-
+echo " ->" . class_exists('SyncSemaphore');
 ?>
