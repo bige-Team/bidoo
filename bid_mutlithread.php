ef960182d30d924f8e2319648365cc574117596d
federicodebona@hotmail.it <Rt9du2pg>
2019-11-11 00:04:30 +0100

    Update bid_multithread

--- bid_mutlithread.php
+++ bid_mutlithread.php
@@ -3,13 +3,37 @@
 include_once "bid_utils.php";
 set_time_limit(0);
 //start();
-$t1 = microtime(true);
 
-$ids = array();
+$auctions = array();
 $toRemove = array();
 
-$ids = getBids();
-print_r($ids);
+$auctions = get_auctions();
+print_r($auctions);
+
+#CREATING SHARED MEMORY SEGMENT
+$a = "";
+foreach ($auctions as $key => $value) 
+{
+		$a .= $value . "|";
+}
+$a = substr($a, 0, -1);//Remove last pipe
+$shm_key = ftok(__FILE__, 'g');
+$shm_id = shmop_open($shm_key, "c", 0644, strlen($a));
+shmop_write($shm_id, $a, 0);
+
+#TEST
+//$start = $arr_size - strlen(array_pop($auctions))*8;
+//echo "start: " . $start . "\n";
+$size = shmop_size($shm_id);
+$count = strlen(array_pop($auctions));
+$start = $size-1-strlen(array_pop($auctions));
+echo "size: $size\nstart: $start\ncount: $count\n";
+$data = shmop_read($shm_id, 0, $size);
+echo "data->" . $data . "\n";
+#TEST
+
+shmop_delete($shm_id);
+
 /*
 for($i = 0; $i < 5; $i++)
 {	
@@ -23,49 +47,9 @@ for($i = 0; $i < 5; $i++)
 }
 
 while(pcntl_waitpid(0, $status) != -1);
-echo "time: " . (microtime(true)-$t1)*1000 . "\n";
 */
 function execute_code($pc)
 {
-	$t1 = microtime(true);
-	$s = "";
-	switch ($pc) {
-		case 0: $s = "https://it.bidoo.com/auction.php?a=Buono_Carrefour_10_e_20P_8723399";
-			break;
-		case 1: $s = "https://it.bidoo.com/auction.php?a=Buono_Amazon_70_e_140P_8721960";
-			break;
-		case 2: $s = "https://it.bidoo.com/auction.php?a=Buono_Amazon_30_e_75P_8722401";
-			break;
-		case 3: $s = "https://it.bidoo.com/auction.php?a=Buono_Amazon_5_e_10P_8716283";
-			break;
-		case 4: $s = "https://it.bidoo.com/auction.php?a=Buono_Carburante_10_e_24P_8723177";
-			break;
-	}
-	$file = file_get_contents($s);
-	echo "[$pc]: Done " . (microtime(true)-$t1)*1000 . "\n";
+
 }
-
-//-------------------------------------------------------------------
-function getBids() {
-		$str = file_get_contents("https://it.bidoo.com");
-
-		$temp = explode("pic_prd", $str);
-		for ($i=0; $i <count($temp) ; $i++) {
-			$start = strlen($temp[$i])-80;
-			$temp[$i] = substr($temp[$i], $start);
-
-			$temp1[$i] = explode("href='auction.php?a=", $temp[$i]);
-
-			$temp2[$i] = explode("'", $temp1[$i][1]);
-			$links[$i] = $temp2[$i][0];
-
-			$temp3 = explode("_", $links[$i]);
-			$ids[$temp3[count($temp3)-1]] = $links[$i];
-			
-		}
-		array_pop($ids);	//rimuovo l'ultimo elemento che è vuoto
-
-		return $ids;
-	}
-
 ?>
--- bid_utils.php
+++ bid_utils.php
@@ -4,8 +4,32 @@ function array_byte_size($arr)
 	$tot = 0;
 	foreach ($arr as $key => $value) 
 	{
-		$tot += strlen($value)*8;
+		$tot += strlen($value);
 	}
 	return $tot;
+}
+
+//old getIds()
+function get_auctions() 
+{
+	$str = file_get_contents("https://it.bidoo.com");
+
+	$temp = explode("pic_prd", $str);
+	for ($i=0; $i <count($temp) ; $i++) 
+	{
+		$start = strlen($temp[$i])-80;
+		$temp[$i] = substr($temp[$i], $start);
+
+		$temp1[$i] = explode("href='auction.php?a=", $temp[$i]);
+
+		$temp2[$i] = explode("'", $temp1[$i][1]);
+		$links[$i] = $temp2[$i][0];
+
+		$temp3 = explode("_", $links[$i]);
+		$ids[$temp3[count($temp3)-1]] = $links[$i];
+	}
+	array_pop($ids);	//rimuovo l'ultimo elemento che è vuoto
+
+	return $ids;
 }
 ?>
