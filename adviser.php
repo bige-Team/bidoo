46c4fd7cd2fb6849a00bb59468bb28ab0c1f6403
Federico <federicodebona@hotmail.it>
2019-11-19 17:28:52 +0100

    .

--- adviser.php
+++ adviser.php
@@ -2,5 +2,6 @@
 include_once "mysql_utils.php";
 $name = explode('=', $_SERVER['QUERY_STRING'])[1];
 echo "$name";
+echo "<a href='https://it.bidoo.com/auction.php?a=$name'>$name</a><br>";
 create_html_table($name);
 ?>
