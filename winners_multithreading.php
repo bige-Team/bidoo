05478f689e92b91d6473b395de75fc78620f1648
Federico <federicodebona@hotmail.it>
2019-11-23 08:25:59 +0100

    .

--- winners_multithreading.php
+++ winners_multithreading.php
@@ -1,8 +1,10 @@
 <?php
+include_once "bid_utils.php";
+
 $val = 8714000; //8.714.000
 $diff = 10000; //10.000
 set_time_limit(0);
-for($i = 0; $i < 30; $i++)
+for($i = 0; $i < 20; $i++)
 {
 	$pid = pcntl_fork();
 	if($pid == -1)
@@ -22,7 +24,7 @@ function execute_code($val, $diff, $x)
 
 	for($i = ($val - ($diff*($x+1))); $i < ($val - ($diff*$x)); $i++)
 	{
-		$s = file_get_contents('https://it.bidoo.com/data.php?ALL='.$i.'&LISTID=0');
+		$s = @file_get_contents('https://it.bidoo.com/data.php?ALL='.$i.'&LISTID=0', false, get_stream_context(1));
 		if(strpos($s, 'OFF') == true) {
 			//echo 'asta finita';
 			$fine = explode(';', $s);
