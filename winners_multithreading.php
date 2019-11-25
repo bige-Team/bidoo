ce4dc271301fcc8f58eb4c58c04067037ca645b9
Ubuntu <ubuntu@ip-172-31-40-104.us-east-2.compute.internal>
2019-11-25 06:17:56 +0000

    .

--- winners_multithreading.php
+++ winners_multithreading.php
@@ -5,13 +5,13 @@ $n_thread = 20;
 $opts = array('http'=>array('timeout'=>1,));
 $ctx = stream_context_create($opts);
 set_time_limit(0);
-for($i = 1+$offset; $i <= $n_thread; $i++)
+for($i = 1; $i <= $n_thread; $i++)
 {
 	$pid = pcntl_fork();
 	if($pid == -1)
 		die("Error forking...\n");
 	elseif($pid == 0){
-		child_loop($n_thread, $i, $ctx);
+		child_loop($n_thread, $i+$offset, $ctx);
 		exit();
 	}
 }
