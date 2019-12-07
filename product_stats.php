da3ad76f650898499ba4dec4cacc15ad77598cab
Federico <federicodebona@hotmail.it>
2019-12-07 15:56:34 +0100

    .

--- product_stats.php
+++ product_stats.php
@@ -12,15 +12,13 @@ if(isset($_REQUEST['btnOK']))
 	$value = $_REQUEST['value'];
 	$l = connect_to_stats();
 	$res = $l->query("select
-				    `a`.`name` AS `name`,
-				    `a`.`id` AS `id`,
-				    `a`.`analized` AS `analized`,
-				    `a`.`assigned` AS `assigned`,
-				    `a`.`terminated` AS `terminated`
-				from
-				    `bidoo_stats`.`auction_tracking` `a`
-				where
-				    (`a`.`name` like concat((select `p`.`products` from `bidoo_stats`.`v_products` `p` where ((`p`.`products` like '%$product%') and (`p`.`products` like '%\_$value\_%'))), '%'))");
+					    `a`.`name` AS `name`,
+					    `a`.`id` AS `id`
+					from
+					    `auction_tracking` `a`
+					where
+					    ((`a`.`name` like '%$product%')
+					    and (`a`.`name` like '%$value\_%'))");
 	$l->close();
 	$res = $res->fetch_all();
 	print_r($res);
