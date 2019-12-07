766048f833ea888e75ca05e2708ff79c127e94b6
Federico <federicodebona@hotmail.it>
2019-12-07 16:09:15 +0100

    .

--- product_stats.php
+++ product_stats.php
@@ -13,7 +13,8 @@ if(isset($_REQUEST['btnOK']))
 	$l = connect_to_stats();
 	$res = $l->query("select
 					    `a`.`name` AS `name`,
-					    `a`.`id` AS `id`
+					    `a`.`id` AS `id`,
+					    `a`.`terminated` AS `terminated`
 					from
 					    `auction_tracking` `a`
 					where
@@ -22,6 +23,27 @@ if(isset($_REQUEST['btnOK']))
 	$l->close();
 	$res = $res->fetch_all();
 	print_r($res);
+
+	$l = connect();
+	$all_prices = array();
+	foreach ($res as $key => $value)
+	{
+		$table_name = $value[0];
+		$is_terminated = $value[2];
+		if($is_terminated == TRUE)
+		{
+			$res = $l->query("SELECT n_puntate FROM $table_name ORDER BY n_puntate DESC LIMIT 1");
+			$all_prices[] = $res->fetch_all()[0][0];
+		}
+	}
+	$l->close();
+	$avg_price = 0;
+	foreach ($all_prices as $val)
+	{
+		$avg_price += $val;
+	}
+	$avg_price /= count($all_prices);
+	echo "<br><b>PREZZO MEDIO PER $product $value: </b> $avg_price EURO<br>";
 }
 
 
