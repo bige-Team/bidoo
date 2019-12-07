1249bb638c16e86307d38619962af33714cb1123
Federico De Bona <federicodebona@users.noreply.github.com>
2019-12-07 18:55:54 +0100

    .

--- product_stats.php
+++ product_stats.php
@@ -18,12 +18,12 @@ if(isset($_REQUEST['btnOK']))
 					from
 					    `auction_tracking` `a`
 					where
-					    ((`a`.`name` like '%amazon%')
-					    and (`a`.`name` like '%10\_%')
+					    ((`a`.`name` like '%$product%')
+					    and (`a`.`name` like '%$value\_%')
 					    and (`a`.`terminated` = 1))");
 	$l->close();
 	$res = $res->fetch_all();
-	print_r($res);
+
 	$l = connect();
 	$all_prices = array();
 	$all_timestamp = array();
@@ -50,7 +50,7 @@ if(isset($_REQUEST['btnOK']))
 		}
 		$avg_price = ($avg_price/count($all_prices))/100;
 		echo "<br><b>PREZZO MEDIO: </b>". round($avg_price, 2) . " EURO<br>";
-		/*
+		
 		$group_by_time = array();
 		for($i=12; $i < 24; $i++)
 		{ 
@@ -76,7 +76,7 @@ if(isset($_REQUEST['btnOK']))
 				echo date("H:i:s", $avg_per_hour) . "<br>";
 			}
 		}
-		*/
+		
 	}
 	else
 		echo "<br><b>PRODOTTO NON TROVATO!</b><br>";	
