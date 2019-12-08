8e87448e2f954a61160492fd84701a51b18184bd
Federico De Bona <federicodebona@users.noreply.github.com>
2019-12-08 14:53:45 +0100

    .

--- product_stats.php
+++ product_stats.php
@@ -84,7 +84,7 @@ if(isset($_REQUEST['btnOK']))
 		$l->close();
 		foreach ($puntate_per_hour as $key => $value)
 		{
-			$puntate_per_hour[$key] = round($value/count($auction_names));
+			$puntate_per_hour[$key] = round($value/count($all_prices));
 		}
 		ksort($puntate_per_hour); #Array ( [11] => 502 [12] => 282...)
 		echo "<br>";
