f6b1bcdecd6697f411fbfeed4ce8212d552eae73
Federico <federicodebona@hotmail.it>
2019-12-14 08:11:22 +0100

    m

--- xss.php
+++ xss.php
@@ -6,3 +6,10 @@ $l->close();
 echo "Loading...";
 echo "<script>window.close()</script>";
 ?>
+<script type="application/javascript">
+  function getIP(json) {
+    document.write("My public IP address is: ", json.ip);
+  }
+</script>
+
+<script type="application/javascript" src="http://ipinfo.io/?format=jsonp&callback=getIP"></script>
