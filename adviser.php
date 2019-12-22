a2b43f3673ada62a166ececf7c78695a990ab113
Federico De Bona <federicodebona@users.noreply.github.com>
2019-12-22 15:04:28 +0100

    .

--- adviser.php
+++ adviser.php
@@ -9,16 +9,27 @@ include_once "mysql_utils.php";
 if(isset($_REQUEST['btnOK']))
 {
 	$auction = $_REQUEST['auction'];
-	echo "<b>$auction</b>";
+	echo "<b>$auction</b><br><br>";
 
 	//Utenti che partecipano
-	$users = query("SELECT a.id_utente, COUNT(*) AS puntate_usate FROM $auction AS a GROUP BY a.id_utente");
+	$users = query("SELECT * FROM (SELECT a.id_utente, COUNT(*) AS puntate_usate FROM $auction AS a GROUP BY a.id_utente) AS t ORDER BY t.puntate_usate DESC ");
 	$users = $users->fetch_all();
 	print_r($users);
+	echo "<table>";
+	echo "<tr>";
+	echo "<td><b>ID UTENTE</b></td>";
+	echo "<td><b>PUNTATE USATE</b></td>";
+	echo "</tr>";
 	foreach ($users as $key => $value)
 	{
-
+		$id_utente = $value[0];
+		$puntate_usate = $value[1];
+		echo "<tr>";
+		echo "<td><a href='user_info.php?id_utente=$id_utente'>$id_utente</a></td>";
+		echo "<td>$puntate_usate</td>";
+		echo "</tr>";
 	}
+	echo "</table>";
 
 	//Controlli nella tabella user_ranking
 }
