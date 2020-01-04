f89e7675a5c2686f49174c18c540f47d0ea39666
Federico De Bona <federicodebona@users.noreply.github.com>
2020-01-04 22:22:17 +0100

    .

--- user_info.php
+++ user_info.php
@@ -5,8 +5,15 @@ if(isset($_REQUEST['id_utente']))
 	include "mysql_utils.php";
 
 	$id_utente = $_REQUEST['id_utente'];
-	echo "<h3>$id_utente</h3>";
-	
+	echo "<h3>$id_utente</h3><br><br>";
+
+	$res = query_to_bidoo_stats("SELECT u.puntate_usate, u.aste_partecipate FROM users_ranking AS u WHERE u.id_utente='$id_utente'");
+	$res = $res->fetch_all();
+	$puntate_usate = $res[0][0];
+	$aste_partecipate = $res[0][1];
+
+	echo "<b>PUNTATE USATE: </b>$puntate_usate<br>";
+	echo "<b>ASTE PARTECIPATE: </b>$aste_partecipate";
 }
 else
 {
