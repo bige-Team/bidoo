b855cd9f110787a50380bcd76d76bb2f502a44f3
Federico De Bona <federicodebona@hotmail.it>
2020-01-08 09:43:38 +0100

    .

--- user_info.php
+++ user_info.php
@@ -11,7 +11,7 @@ if(isset($_REQUEST['id_utente']))
 	$res = $res->fetch_all();
 
 	//Take win auctions
-	$wins = query_to_bidoo_stats("SELECT w.id_asta, w.id_utente, w.time_stamp, w.n_puntate, w.tipo_puntata FROM winners AS w WHERE w.id_utente='$id_utente'");
+	$wins = query_to_bidoo_stats("SELECT w.id_asta, w.time_stamp, w.n_puntate, w.tipo_puntata FROM winners AS w WHERE w.id_utente='$id_utente'");
 	$wins = $wins->fetch_all();
 
 	if(isset($res[0]))
@@ -23,7 +23,28 @@ if(isset($_REQUEST['id_utente']))
 		echo "<b>ASTE PARTECIPATE: </b>$aste_partecipate<br>";
 		echo "<b>MEDIA PUNTATE PER ASTA: </b>" . ($puntate_usate/$aste_partecipate) . "<br>";
 		echo "<br><br>";
-		echo "<b>ASTE VINTE</b>";
+		echo "<b>ASTE VINTE</b><br>";
+		echo "<table>";
+		echo "<tr>";
+		echo "<td>ID ASTA</td>";
+		echo "<td>TIMESTAMP</td>";
+		echo "<td>NUMERO PUNTATE</td>";
+		echo "<td>TIPO PUNTATA</td>";
+		echo "</tr>";
+		foreach ($wins as $key => $value) 
+		{
+			$id_asta = $value[0];
+			$time_stamp = $value[1];
+			$n_puntate = $value[2];
+			$tipo_puntata = $value[3];
+			echo "<tr>";
+			echo "<td><a href='https://it.bidoo.com/auction.php?a=$id_asta'>$id_asta</a></td>";
+			echo "<td>$time_stamp</td>";
+			echo "<td>$n_puntate</td>";
+			echo "<td>$tipo_puntata</td>";
+			echo "</tr>";
+		}
+		echo "</table>";
 		print_r($wins);
 	}
 	else
